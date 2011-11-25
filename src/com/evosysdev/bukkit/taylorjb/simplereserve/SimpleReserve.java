60a67e8a7ae7d2dca6ae0886742b7388d8243370
taylorjb <taylorjb@ee9ebe8e-1be5-6e41-b6f4-7136b78df094>
2011-11-25 05:08:48 +0000

    Added pre-bukkitdev code Removed hooks into Permissions, use bukkit's player.hasPermission

---
+++ src/com/evosysdev/bukkit/taylorjb/simplereserve/SimpleReserve.java
@@ -0,0 +1,76 @@
+package com.evosysdev.bukkit.taylorjb.simplereserve;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleReserve extends JavaPlugin
+{
+    // create the listener
+    private final SimpleReservePlayerListener playerListener = new SimpleReservePlayerListener(this);
+    
+    private ReserveType reserveMethod; // method to use for reserve slots
+
+    /**
+     * set up the block listener and Permissions on enable
+     */
+    public void onEnable()
+    {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_LOGIN, playerListener, Priority.Highest, this);
+
+        try
+        {
+            reserveMethod = ReserveType.valueOf(getConfig().getString("reserve.type").toUpperCase());
+        }
+        catch (NullPointerException npe)
+        { // should happen on first-run when config doesn't exist
+            getConfig().set("reserve.type", "both");
+            getConfig().set("reserve.types", "full,kick,both,none"); // tell people the options
+            saveConfig();
+
+            reserveMethod = ReserveType.valueOf(getConfig().getString("reserve.type").toUpperCase());
+        }
+        catch (IllegalArgumentException iae)
+        { // config not set right(enum can't be valueOf'd)
+            getConfig().set("reserve.type", "both"); // config not set right, default to both
+            saveConfig();
+
+            reserveMethod = ReserveType.valueOf(getConfig().getString("reserve.type").toUpperCase());
+        }
+
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " enabled!");
+    }
+
+    /**
+     * plugin disabled
+     */
+    public void onDisable()
+    {
+        System.out.println("SimpleReserve disabled!");
+    }
+
+    /**
+     * Get the method we are using for reserve slots
+     * 
+     * @return reserve method
+     */
+    public ReserveType getReserveMethod()
+    {
+        return reserveMethod;
+    }
+
+    /**
+     * enum for type of reserve we are using(kick, full bypass, or either(both))
+     * 
+     * @author TJ
+     * 
+     */
+    public enum ReserveType
+    {
+        KICK, FULL, BOTH, NONE
+    }
+}
---
+++ src/com/evosysdev/bukkit/taylorjb/simplereserve/SimpleReservePlayerListener.java
@@ -0,0 +1,59 @@
+package com.evosysdev.bukkit.taylorjb.simplereserve;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class SimpleReservePlayerListener extends PlayerListener
+{
+    private SimpleReserve plugin; // plugin using the listener
+
+    /**
+     * Initialize the player listener
+     * 
+     * @param plugin
+     *            plugin using the listener
+     */
+    public SimpleReservePlayerListener(SimpleReserve plugin)
+    {
+        this.plugin = plugin;
+    }
+
+    @Override
+    /**
+     * If server's full, check if player has a reserve slot, let through if they do
+     * @param event the login event
+     */
+    public void onPlayerLogin(PlayerLoginEvent event)
+    {
+        // full and has permissions to enter
+        if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL && (event.getPlayer().hasPermission("simplereserve.enter.full") || event.getPlayer().hasPermission("simplereserve.enter.kick")))
+        {
+            // enter full method
+            if ((plugin.getReserveMethod() == SimpleReserve.ReserveType.BOTH || plugin.getReserveMethod() == SimpleReserve.ReserveType.FULL) && event.getPlayer().hasPermission("simplereserve.enter.full"))
+            {
+                event.allow();
+                System.out.println("[SimpleReserve] Allowed player " + event.getPlayer().getDisplayName() + " to join full server!");
+            }
+            // enter by kicking another
+            else if ((plugin.getReserveMethod() == SimpleReserve.ReserveType.BOTH || plugin.getReserveMethod() == SimpleReserve.ReserveType.KICK) && event.getPlayer().hasPermission("simplereserve.enter.kick"))
+            {
+                Player[] players = plugin.getServer().getOnlinePlayers();
+                for (Player p : players)
+                {
+                    // player does not have kick prevent power
+                    if (!p.hasPermission("simplereserve.kick.prevent"))
+                    {
+                        p.kickPlayer("Kicked to make room for reserved user!");
+                        event.allow();
+                        System.out.println("[SimpleReserve] Allowed player " + event.getPlayer().getDisplayName() + " to join full server by kicking player " + p.getDisplayName() + "!");
+                        return; // found and kicked a player, let the reserved player join, exit loop
+                    }
+                }
+                
+                // if we get here, no kickable player found...that would be unfortunate.
+                event.setKickMessage("Unable to find any kickable players to open spots!");
+            }
+        }
+    }
+}

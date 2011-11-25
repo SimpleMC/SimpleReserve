75a3e1b4aab92ffb7d50341d8792d03efb0c27d0
taylorjb <taylorjb@ee9ebe8e-1be5-6e41-b6f4-7136b78df094>
2011-11-25 05:14:51 +0000

    Changed to Logger, not sysout

--- src/com/evosysdev/bukkit/taylorjb/simplereserve/SimpleReservePlayerListener.java
+++ src/com/evosysdev/bukkit/taylorjb/simplereserve/SimpleReservePlayerListener.java
@@ -1,5 +1,7 @@
 package com.evosysdev.bukkit.taylorjb.simplereserve;
 
+import java.util.logging.Logger;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -33,7 +35,7 @@ public class SimpleReservePlayerListener extends PlayerListener
             if ((plugin.getReserveMethod() == SimpleReserve.ReserveType.BOTH || plugin.getReserveMethod() == SimpleReserve.ReserveType.FULL) && event.getPlayer().hasPermission("simplereserve.enter.full"))
             {
                 event.allow();
-                System.out.println("[SimpleReserve] Allowed player " + event.getPlayer().getDisplayName() + " to join full server!");
+                Logger.getLogger("minecraft").info("Allowed player " + event.getPlayer().getDisplayName() + " to join full server!");
             }
             // enter by kicking another
             else if ((plugin.getReserveMethod() == SimpleReserve.ReserveType.BOTH || plugin.getReserveMethod() == SimpleReserve.ReserveType.KICK) && event.getPlayer().hasPermission("simplereserve.enter.kick"))
@@ -46,7 +48,7 @@ public class SimpleReservePlayerListener extends PlayerListener
                     {
                         p.kickPlayer("Kicked to make room for reserved user!");
                         event.allow();
-                        System.out.println("[SimpleReserve] Allowed player " + event.getPlayer().getDisplayName() + " to join full server by kicking player " + p.getDisplayName() + "!");
+                        Logger.getLogger("minecraft").info("Allowed player " + event.getPlayer().getDisplayName() + " to join full server by kicking player " + p.getDisplayName() + "!");
                         return; // found and kicked a player, let the reserved player join, exit loop
                     }
                 }
